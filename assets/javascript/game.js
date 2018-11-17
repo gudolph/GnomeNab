8a528ec2edad58afe9cda0873fc81fdc9553965f
Andrew Waterhouse <gudolph@gmail.com>
2018-11-16 23:43:11 -0700

    This seems to be doing all I want functionally.  Just need to make it prettier

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,27 @@
-
-// Assign variables
-var targetValue = "";
-var value;
-var wins = 0;
-var losses = 0;
-var counter = 0;
-
-
 $(document).ready(function () {
 
+    var wins = 0;
+    var losses = 0;
+
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#replay").hide();
+    $("#winner").hide();
+    $("#loser").hide();
     // Create a for-loop to add random integers to the worthArray;
     $("#button").on("click", function () {
 
+        var targetValue = 0;
         var worthArray = [];
+        var gnomeArray = [];
+        var counter = 0;
+        $("#currentvalue").text(targetValue);
+        $("#gnomeValue").text(counter);
+
+        console.log(counter)
 
         for (var i = 0; i < 4; i++) {
-            value = Math.ceil(Math.random() * 14) + 1;
+            value = Math.ceil(Math.random() * 9) + 1;
             worthArray.push(value);
         }
 
@@ -26,11 +32,10 @@ $(document).ready(function () {
             worthArraySum += worthArray[h];
         }
 
-        targetValue = Math.ceil(Math.random() * 75) + worthArraySum;
+        targetValue = Math.ceil(Math.random() * 30) + (worthArraySum);
         $("#currentvalue").text(targetValue);
+        console.log(targetValue)
         for (var j = 0; j < worthArray.length; j++) {
-
-            var gnomeArray = [];
 
             gnomeArray[j] = $("<img>");
             gnomeArray[j].addClass("gnome");
@@ -39,27 +44,50 @@ $(document).ready(function () {
 
             gnomeArray.push(gnomeArray[j]);
             $("#gnomes").append(gnomeArray);
-          
-            if (gnomeArray.length === 4) {
-                $("#button").attr("disabled", true);
-            }
-        }
-        console.log(worthArray[0]);
 
+        }
+
+        console.log(worthArray[0]);
+        $(this).prop("disabled", true);
         $(".gnome").on("click", function () {
             var gnomeValue = ($(this).attr("data-gnomeValue"));
             gnomeValue = parseInt(gnomeValue);
             counter += gnomeValue;
-            $("#gnomevalue").text(counter);
+            $("#gnomeValue").text(counter);
+            console.log(counter);
 
             if (counter === targetValue) {
-                alert("You nabbed dem gnomes!")
                 wins++;
+                $("#winner").show();
+                $("#wins").text(wins);
+
             } else if (counter >= targetValue) {
-                alert("Those gnomes are too valuable, loser!")
+                $("#loser").show();
                 losses++;
+                $("#losses").text(losses);
             }
+            if ((counter === targetValue) || (counter >= targetValue)) {
+                $(".intro").hide();
+                $("#button").hide();
+                $("#replay").show();
+
+            }
+            $("#replay").on("click", function () {
+                $("#wins").text(wins);
+                $("#losses").text(losses);
+                $("#replay").hide();
+                $("#button").show();
+                $("#winner").hide();
+                $("#loser").hide();
+                $(".intro").show();
+                $("#button").prop("disabled", false);
+                $("#gnomeValue").empty();
+                $("#gnomes").empty();
+                $("#currentvalue").empty();
+
+            });
         });
+
     });
 
 });
